f524b82cc04f67a7b1c1834d9cc120acb9fde4f8
Ayushman Thakur <thakurayushman333@gmail.com>
2025-05-22 10:13:55 +0530

    env created

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDF8EkcGpw0vb7ngQgsNas3M5_GzzJzqRs",
-  authDomain: "gamified-habbit-tracker.firebaseapp.com",
-  projectId: "gamified-habbit-tracker",
-  storageBucket: "gamified-habbit-tracker.firebasestorage.app",
-  messagingSenderId: "16602067104",
-  appId: "1:16602067104:web:4d33a256ae3863fb3c99c5",
-  measurementId: "G-ZYS31SMZ2D",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
